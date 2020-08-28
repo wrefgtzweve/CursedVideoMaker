c7969f6b7c5933da7ecce97894b8ca51c8ab2f9e
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2020-08-28 05:57:31 +0200

    Cleaned up code & added a hex signature.

--- CVM/Form1.cs
+++ CVM/Form1.cs
@@ -9,10 +9,24 @@ namespace CVM
         public static string filelocation = "";
         public static string fileoutput = "";
         public static string hex = "";
+        public static byte[] bytes;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static byte[] watermark(byte[] input)
+        {
+            int pos = -88;
+            Array.Resize(ref input, input.Length + 88);
+            int[] mark = { 32, 68, 117, 114, 97, 116, 105, 111, 110, 32, 101, 100, 105, 116, 101, 100, 32, 117, 115, 105, 110, 103, 32, 67, 86, 77, 32, 71, 105, 116, 104, 117, 98, 32, 117, 114, 108, 58, 32, 104, 116, 116, 112, 115, 58, 47, 47, 103, 105, 116, 104, 117, 98, 46, 99, 111, 109, 47, 119, 114, 101, 102, 103, 116, 122, 119, 101, 118, 101, 47, 67, 117, 114, 115, 101, 100, 86, 105, 100, 101, 111, 77, 97, 107, 101, 114 };
+            foreach (byte bit in mark)
+            {
+               pos++;
+               input[input.Length + pos] = bit;
+            };
+            return input;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -56,33 +70,35 @@ namespace CVM
                 foreach (int num2 in bytes)
                 {
                     ++num1;
-                    if (num2 == 109 & bytes[num1 + 1] == (byte)118 & bytes[num1 + 2] == (byte)104 & bytes[num1 + 3] == (byte)100)
+                    if (num2 == 109 & bytes[num1 + 1] == 118 & bytes[num1 + 2] == 104 & bytes[num1 + 3] == 100)
                         break;
                 }
-                if (this.customduration.Checked)
+                if (customduration.Checked)
                 {
-                    int num2 = Decimal.ToInt32(this.timeHour.Value * 3600M + this.timeMinute.Value * 60M + this.timeSecond.Value) * 1000;
-                    bytes[num1 + 16] = (byte)0;
-                    bytes[num1 + 17] = (byte)0;
-                    bytes[num1 + 18] = (byte)3;
-                    bytes[num1 + 19] = (byte)232;
+                    int num2 = Decimal.ToInt32(timeHour.Value * 3600 + timeMinute.Value * 60 + timeSecond.Value) * 1000;
+                    bytes[num1 + 16] = 0;
+                    bytes[num1 + 17] = 0;
+                    bytes[num1 + 18] = 3;
+                    bytes[num1 + 19] = 232;
                     bytes[num1 + 20] = (byte)(num2 >> 24);
                     bytes[num1 + 21] = (byte)(num2 >> 16);
                     bytes[num1 + 22] = (byte)(num2 >> 8);
                     bytes[num1 + 23] = (byte)num2;
-                    int num3 = (int)MessageBox.Show("Successfully converted the video to the specified time.");
+                    bytes = watermark(bytes);
+                    MessageBox.Show("Successfully converted the video to the specified time.");
                 }
-                else if (this.longestpossible.Checked)
+                else if (longestpossible.Checked)
                 {
-                    bytes[num1 + 16] = (byte)0;
-                    bytes[num1 + 17] = (byte)0;
-                    bytes[num1 + 18] = (byte)0;
-                    bytes[num1 + 19] = (byte)1;
-                    bytes[num1 + 20] = (byte)127;
+                    bytes[num1 + 16] = 0;
+                    bytes[num1 + 17] = 0;
+                    bytes[num1 + 18] = 0;
+                    bytes[num1 + 19] = 1;
+                    bytes[num1 + 20] = 127;
                     bytes[num1 + 21] = byte.MaxValue;
                     bytes[num1 + 22] = byte.MaxValue;
                     bytes[num1 + 23] = byte.MaxValue;
-                    int num2 = (int)MessageBox.Show("Successfully converted the video to the longest possible time.");
+                    bytes = watermark(bytes);
+                    MessageBox.Show("Successfully converted the video to the longest possible time.");
                 }
                 else if (this.negativetime.Checked)
                 {
@@ -94,11 +110,12 @@ namespace CVM
                     bytes[num1 + 21] = byte.MaxValue;
                     bytes[num1 + 22] = byte.MaxValue;
                     bytes[num1 + 23] = (byte)240;
-                    int num2 = (int)MessageBox.Show("Successfully converted the video to a negative time.");
+                    bytes = watermark(bytes);
+                    MessageBox.Show("Successfully converted the video to a negative time.");
                 }
                 else
                 {
-                    int num4 = (int)MessageBox.Show("Please select an option");
+                    MessageBox.Show("Please select an option");
                 }
                 if (fileoutput.EndsWith("mp4"))
                 {
@@ -111,13 +128,13 @@ namespace CVM
             }
             else if (filelocation.Length == 0)
             {
-                int num5 = (int)MessageBox.Show("Please select an file input location.");
+                MessageBox.Show("Please select an file input location.");
             }
             else
             {
                 if (fileoutput.Length != 0)
                     return;
-                int num1 = (int)MessageBox.Show("Please select an file output location.");
+                MessageBox.Show("Please select an file output location.");
             }
         }
     }
